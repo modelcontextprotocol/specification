6acdb47af4ed4bc59d12668b4b58bc86ffd83bef
noctonic <user@example.com>
2025-05-12 06:19:34 -0400

    Removed URI restriction from root schema comment.

--- schema/draft/schema.ts
+++ schema/draft/schema.ts
@@ -1236,9 +1236,7 @@ export interface ListRootsResult extends Result {
  */
 export interface Root {
   /**
-   * The URI identifying the root. This *must* start with file:// for now.
-   * This restriction may be relaxed in future versions of the protocol to allow
-   * other URI schemes.
+   * The URI identifying the root.
    *
    * @format uri
    */
