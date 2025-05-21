3062586cfd4ed9471607dd259ed6475e7bb25c46
Luca Chang <lucalc@amazon.com>
2025-05-21 10:38:44 -0700

    chore: fix schema after bad rebase

--- schema/draft/schema.ts
+++ schema/draft/schema.ts
@@ -683,19 +683,6 @@ export interface ListToolsResult extends PaginatedResult {
   tools: Tool[];
 }
 
-/**
- * The server's response to a tool call.
- *
- * Any errors that originate from the tool SHOULD be reported inside the result
- * object, with `isError` set to true, _not_ as an MCP protocol-level error
- * response. Otherwise, the LLM would not be able to see that an error occurred
- * and self-correct.
- *
- * However, any errors in _finding_ the tool, an error indicating that the
- * server does not support tool calls, or any other exceptional conditions,
- * should be reported as an MCP error response.
- */
-
 export type Content =
   | TextContent
   | ImageContent
